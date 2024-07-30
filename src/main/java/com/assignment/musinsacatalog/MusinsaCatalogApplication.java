fa5d1ad57726fc8660b3e6e7ac0c5b0c938349fc
jaejun-ko <goj609@gmail.com>
2024-07-30 17:06:00 +0900

    Initial Commit

---
+++ src/main/java/com/assignment/musinsacatalog/MusinsaCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.musinsacatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusinsaCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assignment/musinsacatalog/MusinsaCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.musinsacatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinsaCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
